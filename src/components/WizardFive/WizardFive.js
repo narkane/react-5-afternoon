7b8547cccb2b4809278354f4e52fd79d0e6a4aa2
Hans Thummel <enikath@gmail.com>
2018-12-17 15:57:42 -0600

    completed

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,22 +1,37 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { updateAgent } from "../../ducks/reducer";
 
 class WizardFive extends Component {
-
-    render(){
-        return(
-            <div className="parent-div">
-                    <div className="vert-align">    
-
-                    <p>Are you currently working with a real estate agent?</p> <br />
-                    <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { updateAgent } = this.props;
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          <p>Are you currently working with a real estate agent?</p> <br />
+          <div className="row">
+            <Link to="/wSix">
+              <button onClick={() => updateAgent("Yes")}>Yes</button>
+            </Link>
+            <Link to="/wSix">
+              <button onClick={() => updateAgent("No")}>No </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default WizardFive;
+function mapStateToProps(state) {
+  const { realEstateAgent } = state;
+
+  return { realEstateAgent };
+}
+
+export default connect(
+  mapStateToProps,
+  { updateAgent }
+)(WizardFive);
--- src/components/WizardSix/WizardSix.js
+++ src/components/WizardSix/WizardSix.js
@@ -1,28 +1,45 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { updateCost, updatePayment } from "../../ducks/reducer";
 
 class WizardSix extends Component {
-
-    render(){
-        return(
-            <div className="parent-div">
-                    <div className="vert-align">                    <p>What is the estimated purchase price?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateCost}/> <br />
-
-
-                    <p>How much are you putting down as a down payment?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateDownPayment}/>                    
-                        
-                    
-                    <Link to="/wSeven"><button className="margin-btn"> Next </button></Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { updateCost, updatePayment } = this.props;
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          {" "}
+          <p>What is the estimated purchase price?</p> <br />
+          <input
+            type="text"
+            placeholder="amount"
+            onChange={e => updateCost(e.target.value)}
+          />{" "}
+          <br />
+          <p>How much are you putting down as a down payment?</p> <br />
+          <input
+            type="text"
+            placeholder="amount"
+            onChange={e => updatePayment(e.target.value)}
+          />
+          <Link to="/wSeven">
+            <button className="margin-btn"> Next </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default WizardSix;
+function mapStateToProps(state) {
+  const { cost, downPayment } = state;
+
+  return { cost, downPayment };
+}
+
+export default connect(
+  mapStateToProps,
+  { updateCost, updatePayment }
+)(WizardSix);
